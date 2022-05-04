1b4155459653828087a520c5810202c98f07271c
pushkarwagh <puwagh@bestpeers.com>
2022-05-04 15:57:45 +0530

    complete project

---
+++ src/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const dot_env = require('dotenv');
+const path = require('path');
+var cors = require('cors');
+
+require('./connection.js');
+const routes = require('./routes.js');
+
+const app = express();
+
+app.use(express.static('public'))
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
+dot_env.config();
+const port = process.env.PORT || 8000 ;
+console.log("dote env ==>",process.env.PORT) 
+
+app.listen(port ,() => 
+    console.log( `server listening on ${port}`)
+);
