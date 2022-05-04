d486d8b628c5dfe8c683bc95a8c4f00361ca57e2
pushkarwagh <puwagh@bestpeers.com>
2022-05-04 17:11:53 +0530

    ppr

--- src/server.js
+++ src/server.js
@@ -13,9 +13,9 @@ app.use(express.json());
 app.use(cors());
 app.use(routes);
 
-app.use(express.static(path.resolve(__dirname, "./client/build/")));
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build/", "index.html"));
+  response.sendFile(path.resolve(__dirname, "./client/build/index.html"));
 });
 
 dot_env.config();
